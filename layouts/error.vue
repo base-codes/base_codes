4483097934b8ed67ce0435a66fa759d6800be00f
Pavlenko Vladislav <vladriskis@gmail.com>
2020-12-27 22:12:35 +0400

    404 page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,13 +1,79 @@
 <template>
-  <div>
-    <h1 v-if="error.statusCode === 404">Page not found</h1>
-    <h1 v-else>An error occurred</h1>
-    <NuxtLink to="/">Home page</NuxtLink>
+  <div class="error-wrapper">
+    <div class="error-block">
+      <div class="columns">
+        <div class="column is-offset-3">
+          <h1 v-if="error.statusCode === 404" class="headler-error">
+            404
+          </h1>
+          <h1 v-else>
+            An error occurred
+          </h1>
+        </div>
+      </div>
+      <hr>
+      <div class="columns">
+        <div class="column is-3">
+          <p>Error</p>
+        </div>
+        <div class="column is-6">
+          <p>
+            Unfortunately something went wrong. Please dubble check the URL or
+            go back to the landing page. If you countinue to get this error -
+            please email us at support@everydayexperiments.ikea.com
+          </p>
+        </div>
+        <div class="column is-3">
+          <div class="to-home">
+            <p class="home-button" @click="toHome">
+              Home page
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['error']
+  props: ['error'],
+  methods: {
+    toHome () {
+      this.$router.push('/')
+    }
+  }
 }
 </script>
+
+<style lang="scss">
+.error-wrapper {
+  height: 100vh;
+  padding: 0 64px;
+
+  .error-block {
+    height: 90%;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    margin-top: 88px;
+  }
+  .home-button {
+    padding: 16px 64px;
+    border: 1px solid white;
+    display: inline;
+    box-sizing: border-box;
+    cursor: pointer;
+  }
+  .to-home {
+    display: flex;
+    justify-content: flex-end;
+  }
+  .headler-error {
+    font-size: 256px;
+  }
+  //   .border {
+  //     border: 1px solid white;
+  //   }
+}
+</style>
