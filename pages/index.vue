ebb881a9f20b9221a21e59257a230c594e067170
Pavlenko Vladislav <vladriskis@gmail.com>
2020-12-30 00:41:35 +0400

    huinya

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
         <div class="column is-12">
           <div class="img-box">
             <h1>Технологии и дизайн</h1>
-            <img src="~/assets/imgs/base-gif.gif" />
+            <img src="~/assets/imgs/base-gif.gif">
             <div class="intro-text">
               <p>Разработка</p>
               <p>Продвижение</p>
@@ -18,22 +18,24 @@
     <section class="sec-about">
       <div class="columns">
         <div class="column is-12">
-          <p class="description">разрабатываем, продвигаем, поддерживаем</p>
+          <p class="description">
+            разрабатываем, продвигаем, поддерживаем
+          </p>
         </div>
       </div>
 
       <div class="columns">
         <div class="column is-two-thirds">
           <p>
-            BASE.CODES УЖЕ 5 ЛЕТ НЕСЁТ<br />
-            ЦИФРОВИЗАЦИЮ В МАССЫ. КОМАНДА<br />
-            ЗНАЮЩАЯ РЕАЛИИ СОВРЕМЕННОГО БИЗНЕСА.<br />
-            МЫ ВЗРАСТИМ НОВУЮ КУЛЬТУРУ ВАШЕЙ<br />
-            ОРГАНИЗАЦИИ - КУЛЬТУРУ ТЕХНОЛОГИЙ.<br />
+            BASE.CODES УЖЕ 5 ЛЕТ НЕСЁТ<br>
+            ЦИФРОВИЗАЦИЮ В МАССЫ. КОМАНДА<br>
+            ЗНАЮЩАЯ РЕАЛИИ СОВРЕМЕННОГО БИЗНЕСА.<br>
+            МЫ ВЗРАСТИМ НОВУЮ КУЛЬТУРУ ВАШЕЙ<br>
+            ОРГАНИЗАЦИИ - КУЛЬТУРУ ТЕХНОЛОГИЙ.<br>
           </p>
         </div>
         <div class="column">
-          <h1>Build<br />[Smart]</h1>
+          <h1>Build<br>[Smart]</h1>
         </div>
       </div>
     </section>
@@ -44,7 +46,9 @@
             <p>01</p>
             <p>/04</p>
           </div>
-          <p class="service-title">ВЕБ РАЗРАБОТКА</p>
+          <p class="service-title">
+            ВЕБ РАЗРАБОТКА
+          </p>
           <p class="service-description s-desc">
             Реализуем потребные бизнес-решения используя современные
             веб-технологии. Исследуем предпочтения ваших пользователей и создаём
@@ -53,16 +57,20 @@
           </p>
         </div>
         <div class="column serv-item">
-          <img src="~/assets/imgs/stub.jpg" alt="" />
-          <p class="serv-item-title">ЦИФРОВОЙ СЛЭМ-ДАНК</p>
+          <img src="~/assets/imgs/stub.jpg" alt="">
+          <p class="serv-item-title">
+            ЦИФРОВОЙ СЛЭМ-ДАНК
+          </p>
           <p>
             Реализуем потребные бизнес-решения используя современные
             веб-технологии.
           </p>
         </div>
         <div class="column serv-item">
-          <img src="~/assets/imgs/stub.jpg" alt="" />
-          <p class="serv-item-title">БЫСТРЫЙ СТАРТ ПРОЕКТА</p>
+          <img src="~/assets/imgs/stub.jpg" alt="">
+          <p class="serv-item-title">
+            БЫСТРЫЙ СТАРТ ПРОЕКТА
+          </p>
           <p>
             Реализуем потребные бизнес-решения используя современные
             веб-технологии.
@@ -75,23 +83,29 @@
             <p>02</p>
             <p>/04</p>
           </div>
-          <p class="service-title">БРЕНДИНГ</p>
+          <p class="service-title">
+            БРЕНДИНГ
+          </p>
           <p class="service-description s-desc">
             Веб-интерфейсы, фирменный стиль, гайдлайны, логотипы, индустриальный
             дизайн и многое другое. Задизайним абсолютно всё от А до Я.
           </p>
         </div>
         <div class="column serv-item">
-          <img src="~/assets/imgs/stub.jpg" alt="" />
-          <p class="serv-item-title">АЙДЕНТИФИКАЦИОННЫЙ СКАЧОК</p>
+          <img src="~/assets/imgs/stub.jpg" alt="">
+          <p class="serv-item-title">
+            АЙДЕНТИФИКАЦИОННЫЙ СКАЧОК
+          </p>
           <p>
             Реализуем потребные бизнес-решения используя современные
             веб-технологии.
           </p>
         </div>
         <div class="column serv-item">
-          <img src="~/assets/imgs/stub.jpg" alt="" />
-          <p class="serv-item-title">СТРАТЕГИЧЕСКИЙ КАРТ-БЛАНШ</p>
+          <img src="~/assets/imgs/stub.jpg" alt="">
+          <p class="serv-item-title">
+            СТРАТЕГИЧЕСКИЙ КАРТ-БЛАНШ
+          </p>
           <p>
             Реализуем потребные бизнес-решения используя современные
             веб-технологии.
@@ -104,7 +118,9 @@
             <p>03</p>
             <p>/04</p>
           </div>
-          <p class="service-title">СОЗДАНИЕ КОНТЕНТА</p>
+          <p class="service-title">
+            СОЗДАНИЕ КОНТЕНТА
+          </p>
           <p class="service-description s-desc">
             Реализуем потребные бизнес-решения используя современные
             веб-технологии. Исследуем предпочтения ваших пользователей и создаём
@@ -113,16 +129,20 @@
           </p>
         </div>
         <div class="column serv-item">
-          <img src="~/assets/imgs/stub.jpg" alt="" />
-          <p class="serv-item-title">AR - РАЗРАБОТКА</p>
+          <img src="~/assets/imgs/stub.jpg" alt="">
+          <p class="serv-item-title">
+            AR - РАЗРАБОТКА
+          </p>
           <p>
             Реализуем потребные бизнес-решения используя современные
             веб-технологии.
           </p>
         </div>
         <div class="column serv-item">
-          <img src="~/assets/imgs/stub.jpg" alt="" />
-          <p class="serv-item-title">ПОТРЕБИТЕЛЬСКИЙ ФОКУС</p>
+          <img src="~/assets/imgs/stub.jpg" alt="">
+          <p class="serv-item-title">
+            ПОТРЕБИТЕЛЬСКИЙ ФОКУС
+          </p>
           <p>Иллююстрации, копирайт, графический интерфейс для игр.</p>
         </div>
       </div>
@@ -132,7 +152,9 @@
             <p>04</p>
             <p>/04</p>
           </div>
-          <p class="service-title">ПРОДВИЖЕНИЕ</p>
+          <p class="service-title">
+            ПРОДВИЖЕНИЕ
+          </p>
           <p class="service-description s-desc">
             Реализуем потребные бизнес-решения используя современные
             веб-технологии. Исследуем предпочтения ваших пользователей и создаём
@@ -141,16 +163,20 @@
           </p>
         </div>
         <div class="column serv-item">
-          <img src="~/assets/imgs/stub.jpg" alt="" />
-          <p class="serv-item-title">КОНТЕКСТНАЯ РЕКЛАМА</p>
+          <img src="~/assets/imgs/stub.jpg" alt="">
+          <p class="serv-item-title">
+            КОНТЕКСТНАЯ РЕКЛАМА
+          </p>
           <p>
             Реализуем потребные бизнес-решения используя современные
             веб-технологии.
           </p>
         </div>
         <div class="column serv-item">
-          <img src="~/assets/imgs/stub.jpg" alt="" />
-          <p class="serv-item-title">SEO BUST</p>
+          <img src="~/assets/imgs/stub.jpg" alt="">
+          <p class="serv-item-title">
+            SEO BUST
+          </p>
           <p>
             Реализуем потребные бизнес-решения используя современные
             веб-технологии.
@@ -196,56 +222,56 @@ export default {}
       color: #fff;
     }
   }
+}
 
-  .sec-services {
-    padding-top: 192px;
-    .main-service.column {
-      border-right: 1px solid white;
-      padding-right: 32px !important;
-    }
-    .serv-row {
-      border-top: 1px solid white;
-      padding-top: 32px;
-      &:last-child {
-        border-bottom: 1px solid white;
-      }
-    }
-    .num-service {
-      display: flex;
-      justify-content: space-between;
-      margin-top: 48px;
-      p {
-        font-size: 24px;
-      }
-    }
-    .service-title {
-      font-size: 48px;
-      margin-top: 96px;
-    }
-    .service-description {
-      font-size: 24px;
-      font-weight: 200;
-      margin-top: 96px;
-    }
-
-    .serv-item.column {
-      padding-left: 32px !important;
-      p {
-        display: block;
-        font-size: 24px;
-        margin-top: 24px;
-        font-weight: 200;
-      }
-    }
-
-    .serv-item-title {
-      font-weight: 400 !important;
-    }
+.sec-services {
+  padding-top: 192px;
+  .main-service.column {
+    border-right: 1px solid white;
+    padding-right: 32px !important;
   }
-
-  .sec-about {
+  .serv-row {
     border-top: 1px solid white;
-    border-bottom: 1px solid white;
+    padding-top: 32px;
+    &:last-child {
+      border-bottom: 1px solid white;
+    }
   }
+  .num-service {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 48px;
+    p {
+      font-size: 24px;
+    }
+  }
+  .service-title {
+    font-size: 48px;
+    margin-top: 96px;
+  }
+  .service-description {
+    font-size: 24px;
+    font-weight: 200;
+    margin-top: 96px;
+  }
+
+  .serv-item.column {
+    padding-left: 32px !important;
+    p {
+      display: block;
+      font-size: 24px;
+      margin-top: 24px;
+      font-weight: 200;
+    }
+  }
+
+  .serv-item-title {
+    font-weight: 400 !important;
+  }
+}
+
+.sec-about {
+  border-top: 1px solid white;
+  border-bottom: 1px solid white;
 }
 </style>
