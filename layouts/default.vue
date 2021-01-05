c7d1e02d3b5ab959c36d61f899f14bf39a5acef4
kiki <kiki@yandex.ru>
2021-01-05 14:58:49 +0100

    some grains for gains

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <div class="page-noise" />
     <nav-main />
     <Nuxt />
     <Footer />
@@ -22,6 +23,74 @@ export default {
   box-sizing: border-box;
   user-select: none;
 }
+.page-noise{
+  background: #161616;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  left: 0;
+  top: 0;
+  pointer-events: none;
+  z-index: 0;
+  opacity: .4;
+}
+.page-noise:after {
+    content: "";
+    background-image: url("http://api.thumbr.it/whitenoise-361x370.png?background=ffffffff&noise=9e9e9e&density=43&opacity=42");
+    top: -100%;
+    left: -50%;
+    height: 300%;
+    width: 300%;
+    opacity: 0.2;
+    position: fixed;
+    animation: grains 8s steps(10) infinite;
+}
+@keyframes grains {
+    0% {
+        transform: translate3d(0,9rem,0)
+    }
+
+    10% {
+        transform: translate3d(-1rem,-4rem,0)
+    }
+
+    20% {
+        transform: translate3d(-8rem,2rem,0)
+    }
+
+    30% {
+        transform: translate3d(9rem,-9rem,0)
+    }
+
+    40% {
+        transform: translate3d(-2rem,7rem,0)
+    }
+
+    50% {
+        transform: translate3d(-9rem,-4rem,0)
+    }
+
+    60% {
+        transform: translate3d(2rem,6rem,0)
+    }
+
+    70% {
+        transform: translate3d(7rem,-8rem,0)
+    }
+
+    80% {
+        transform: translate3d(-9rem,1rem,0)
+    }
+
+    90% {
+        transform: translate3d(6rem,-5rem,0)
+    }
+
+    to {
+        transform: translate3d(-7rem,0,0)
+    }
+}
+
 body {
   font-family: "IBM Plex Mono", monospace;
   background-color: #161616;
